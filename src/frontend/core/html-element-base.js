48d9ba967f0cbd24199826ea76c393e6c9b0d59a
Muhammad Mohsen <muhammadaeh@gmail.com>
2025-02-18 18:04:56 +0200

    feat (html element base): add vscode api into `HTMLElementBase`

--- src/frontend/core/html-element-base.js
+++ src/frontend/core/html-element-base.js
@@ -1,9 +1,31 @@
+// @ts-ignore
+const vsc = window.vsc || acquireVsCodeApi();
+// @ts-ignore
+window.vsc = vsc;
+
 export default class HTMLElementBase extends HTMLElement {
 	handle;
 
 	constructor() {
 		super();
+
 		this.handle = `${this.constructor.name}_${Date.now()}`;
 		window[this.handle] = this;
+
+		window.addEventListener('message', (event) => this.onMessage(event));
+	}
+
+	postMessage(msg) {
+		vsc.postMessage(msg);
+	}
+	onMessage(event) {
+		console.log(event);
+	}
+
+	setState(state) {
+		vsc.setState(state);
+	}
+	getState() {
+		return vsc.getState();
 	}
 }
--- src/frontend/core/vsc.js
+++
@@ -1,8 +0,0 @@
-/**
- * @type {{ postMessage: (message: any, transfer: any) => void, setState: (newState: any) => void, getState: () => any }}
- */
-// @ts-ignore
-export const vsc = acquireVsCodeApi();
-
-// @ts-ignore
-window.vsc = vsc;
